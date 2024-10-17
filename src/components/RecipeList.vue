c275ceb55c64f116595c23b906470da84a36f608
mip <michel@spengergasse.at>
2024-10-17 13:30:21 +0200

    added RecipesView works

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,3 +1,29 @@
+<script setup>
+// import { RouterLink } from 'vue-router';
+defineProps(
+  {
+    recipes: {
+      type: Array,
+      required: true
+    }
+  }
+);
+</script>
+
 <template>
-  <h1>RecipesList</h1>
+  <div class="row">
+    <div class="col-md-4" v-for="recipe in recipes" :key="recipe.id">
+      <div class="card h-100 mb-4 shadow-sm">
+        <div class="card-body">
+          <h5 class="card-title">{{ recipe.title }}</h5>
+          <p class="card-text">{{ recipe.description }}</p>
+          <p class="card-text">{{ recipe.duration }} Minuten</p>
+          <!-- <RouterLink :to="{ name: 'RecipeDetails', params: { id: recipe.id } }" class="btn btn-primary">Details -->
+          <!-- </RouterLink> -->
+          <RouterLink :to="`/recipes/${recipe.id}`" class="btn btn-primary">Details
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
---
+++ src/views/RecipesView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import RecipeList from '../components/RecipeList.vue'
+import { recipes } from '@/assets/temp-data'
+// import { recipes } from '../assets/temp-data'
+</script>
+
+<template>
+  <div class="container mt-3">
+    <RecipeList :recipes="recipes" />
+  </div>
+</template>
