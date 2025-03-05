f9cfdd57ee2a264813d1f2fc234bda60599541f5
mip <michel@spengergasse.at>
2025-03-05 13:48:18 +0100

    EDIT  firebaseConfig const from .env (OLD firebase project deleted!!)

--- src/main.js
+++ src/main.js
@@ -12,15 +12,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBM1_lJJuXTETS_G0Fd0Pt3myn1kYQLEGE",
-  authDomain: "yumlog-50f87.firebaseapp.com",
-  projectId: "yumlog-50f87",
-  storageBucket: "yumlog-50f87.firebasestorage.app",
-  messagingSenderId: "730170076140",
-  appId: "1:730170076140:web:d48cbf898dcb4dd1dbeec9",
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+  }
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
